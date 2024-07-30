a09fe54aab734aa32ed705e95f85967277a108b8
sirdebar <semkochkin@gmail.com>
2024-07-30 19:38:03 +0300

    Initial commit

---
+++ nextpizza/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Checkbox } from "./checkbox";
+export { Button } from "./button";
+export { Dialog } from "./dialog";
+export { Drawer } from "./drawer";
+export { Input } from "./input";
+export { Popover } from "./popover";
+export { Select } from "./select";
+export { Skeleton } from "./skeleton";
+export { Slider } from "./slider";
